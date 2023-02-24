edbb2b869a6a5973f0b32c215be9ad8be541c4cd
jplorenzini <51427975+josepedrolorenzini@users.noreply.github.com>
2023-02-24 14:06:12 +1100

    Update lesson4.php

--- oop/lesson4.php
+++ oop/lesson4.php
@@ -1,10 +1,12 @@
 <?php
-
+//https://www.phptutorial.net/php-oop/php-override-method/
+///in lesson 9
 class User{
     
     //properties methods
     public $username;
-    private $email;
+    protected $email;
+    public $role = "member";
 
     public $script ;
 
@@ -35,25 +37,54 @@ class User{
        
     }
 
+    public function message(){
+        return "$this->email sent a new message";
+    }
+
     public function __toString(){
-        return $this->username . " <br>". $this->email;
+        return $this->username . " ". $this->email . " " . $this->role . " <br>";
     }
 
 }
 
-
+///child class AdminUser
 class AdminUser extends User{
+
+    public $level;
+    public $role = "admin";
+    public function __construct($username,$email,$level){
+        
+        $this->level = $level;
+        parent::__construct($username,$email);
+    }
+    
+    public function __toString(){
+        // return $this->username . " <br>". $this->email . " " . $this->level;
+        return parent::__toString() . " " . $this->level . " " ;
+    }
+
+    public function message(){
+        return "$this->email ,  an admin sent a new message";
+    }
+
 
 }
 
 $userone = new User('user 1' , 'user1@mail.com');
 $usertwo = new User('user 2' , 'user2@mail.com');
-$adminUser = new AdminUser("admin User" , "admin@usermail.com");
+$adminUser = new AdminUser("admin User" , "admin@usermail.com", 1981);
+echo $adminUser;
 echo "<br>";
 echo $adminUser->getEmail();
 echo "<br>";
+echo $usertwo;
+echo "<br>";
 echo $userone->getEmail()  . "<br>";
-
+echo "<br>";
+echo $userone->message()  . "<br>";
+echo "<br>";
+echo $adminUser->message()  . "<br>";
+echo "<br>";
 $userone->setEmail('pepepe@mail.com.au');
 echo $userone->getEmail() . "<br>";
 echo $userone->username;
